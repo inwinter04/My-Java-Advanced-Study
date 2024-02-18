24c9823d0dab030c113c6c53da7f3b15de56413b
冬天冬天W <inwinter04@163.com>
2024-02-18 11:34:21 +0800

    💕 完成Day09课程... 数据结构这四个字让我瑟瑟发抖 当你在荒废时间，有多少人在拼命。别在最该奋斗的日子，选择了安逸。

--- day09-code/src/cn/iamdt/collection/list/ArrayListDemo.java
+++ day09-code/src/cn/iamdt/collection/list/ArrayListDemo.java
@@ -2,7 +2,7 @@ package cn.iamdt.collection.list;
 
 import java.util.ArrayList;
 
-public class ArrayListTest {
+public class ArrayListDemo {
     public static void main(String[] args) {
 
         // 创建ArrayList时，默认容量为0，直至添加第一个元素时容量扩容为10
